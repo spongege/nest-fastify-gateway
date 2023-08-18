a9daa809cb6ccec02aefe80df5944c30eb30cf35
fengzhonghui <fengzhonghui@maoyan.com>
2023-08-18 14:18:22 +0800

    feat: 改用 Fastify & 配置全局拦截器

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+import { VERSION_NEUTRAL, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
+import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  app.useGlobalInterceptors(new TransformInterceptor());
+  app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
+  // 接口版本化管理
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: [VERSION_NEUTRAL, '1', '2'],
+  });
+
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/user/user.controller.ts
@@ -0,0 +1,75 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Version,
+  VERSION_NEUTRAL,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { BusinessException } from 'src/common/exceptions/business.exception';
+
+@Controller({
+  path: 'user',
+  // version: '1',
+})
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Get('findError')
+  @Version([VERSION_NEUTRAL, '1'])
+  findError() {
+    const a: any = {};
+    console.log(a.b.c);
+    return this.userService.findAll();
+  }
+
+  @Get('findBusinessError')
+  @Version([VERSION_NEUTRAL, '1'])
+  findBusinessError() {
+    const a: any = {};
+    try {
+      console.log(a.b.c);
+    } catch (error) {
+      throw new BusinessException('你这个参数错了');
+    }
+    return this.userService.findAll();
+  }
+
+  @Get()
+  @Version([VERSION_NEUTRAL, '1'])
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get()
+  @Version('2')
+  findAll2() {
+    return 'i am new one';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.remove(+id);
+  }
+}
