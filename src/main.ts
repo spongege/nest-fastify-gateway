30e0a52e5d021398910cdf76a5fc7bccc345a8d9
fengzhonghui <fengzhonghui@maoyan.com>
2023-08-29 19:15:18 +0800

    feat: nestjs 注册swagger

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import { AppModule } from './app.module';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
 import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as packageConfig from '../package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -22,6 +24,16 @@ async function bootstrap() {
     type: VersioningType.URI,
     defaultVersion: [VERSION_NEUTRAL, '1', '2'],
   });
+
+  const options = new DocumentBuilder()
+    .setTitle(packageConfig.name)
+    .setDescription(packageConfig.description)
+    .setVersion(packageConfig.version)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('/api/doc', app, document);
 
   await app.listen(3000);
 }
